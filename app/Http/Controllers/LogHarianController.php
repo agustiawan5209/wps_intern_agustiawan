7eedd5f58411d78fec2f8f875e87e40802170fda
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-02-22 10:30:36 +0800

    Buat Model dan Controller Log Harian

---
+++ app/Http/Controllers/LogHarianController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LogHarian;
+use App\Http\Requests\StoreLogHarianRequest;
+use App\Http\Requests\UpdateLogHarianRequest;
+
+class LogHarianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLogHarianRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LogHarian $logHarian)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LogHarian $logHarian)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLogHarianRequest $request, LogHarian $logHarian)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LogHarian $logHarian)
+    {
+        //
+    }
+}
