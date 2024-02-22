89b3ba9c2630b160a966472789b6092e33442557
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-02-22 10:39:51 +0800

    Cek Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'jabatan'=> fake()->randomKey(['Direktur', 'Manager', 'Pegawai'])
+            'jabatan'=> fake()->randomElement(['Direktur', 'Manager', 'Staff']),
         ];
     }
 
